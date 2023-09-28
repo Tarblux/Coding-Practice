ff031290ecc3a2922ed743fd9e876fbbf0acdcce
Tariq Williams <121918283+Tarblux@users.noreply.github.com>
2023-09-28 08:30:38 -0500

    26.9 jsandfolderedits

---
+++ Html/index.js
@@ -0,0 +1,42 @@
+//this is my first javascript code 
+console.log("Hello Squirrel");
+
+let name = 'Whipski';
+
+console.log(name);
+
+//Constants
+
+const interestRate = 0.3;
+// interestRate = 9 , This will throw an error since we can't reassign a constant.
+console.log(interestRate)
+
+//Primitives
+
+let age = 30 ; // Number Literal
+let name2 = 'Tariq' ; //String Literal
+let isApproved = false ; // Boolean Literal 
+
+let firstName; // If we don't initialize it by default it will be undefined 
+let lastName = null ; // This clears  the value stored in a variable 
+
+//functions 
+
+function greet(name , lastName) {
+        console.log('Hello ' + name + lastName )
+}
+
+greet('John' , 'Smith')
+
+// Notice that this prints undefined , that's becuase we didn't pass a second argument to the function so the last name parameter in the function ended up
+// being undefined
+
+greet('Mary')
+
+// Function that calculates a value 
+
+function square(number) {
+   return number * number ;     
+}
+
+console.log(square(9))
