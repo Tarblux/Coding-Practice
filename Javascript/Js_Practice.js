37bd37322d21f8922bf4041ad45c84e9cff9a3aa
Tariq Williams <121918283+Tarblux@users.noreply.github.com>
2023-10-12 04:40:33 -0500

    JS Kernel Setup

--- Javascript/Js_Practice.js
+++ Javascript/Js_Practice.js
@@ -1,9 +1,3 @@
 console.log('1syde')
 
-// Use this to install js kernel 
 
-pip3 install --upgrade pip
-pip3 install jupyter
-npm install -g ijavascript
-ijsinstall
-# Or run: %appdata%\npm\ijsinstall
