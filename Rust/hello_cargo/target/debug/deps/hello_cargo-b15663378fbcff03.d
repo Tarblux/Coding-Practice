ffaf8de1af11ba263101f38d8cd56270c8e72cd3
Tariq Williams <121918283+Tarblux@users.noreply.github.com>
2024-04-29 10:26:03 -0500

    First steps with rust.

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d
@@ -0,0 +1,5 @@
+/Users/twilliams/.cdprct/Coding Practice/Rust/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03: src/main.rs
+
+/Users/twilliams/.cdprct/Coding Practice/Rust/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/twilliams/.cdprct/Coding\ Practice/Rust/hello_cargo/target/debug/hello_cargo: /Users/twilliams/.cdprct/Coding\ Practice/Rust/hello_cargo/src/main.rs
