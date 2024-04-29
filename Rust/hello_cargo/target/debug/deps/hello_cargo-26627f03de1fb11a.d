d0466af5ecc1f20349b4cf2ede22e3c5639ffc1a
Tariq Williams <121918283+Tarblux@users.noreply.github.com>
2024-04-29 10:32:44 -0500

    Configuring rust analyzer dependencies and build server

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo-26627f03de1fb11a.d
@@ -0,0 +1,5 @@
+/Users/twilliams/.cdprct/Coding Practice/Rust/hello_cargo/target/debug/deps/libhello_cargo-26627f03de1fb11a.rmeta: src/main.rs
+
+/Users/twilliams/.cdprct/Coding Practice/Rust/hello_cargo/target/debug/deps/hello_cargo-26627f03de1fb11a.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo-d0f922fb585396f1.d
@@ -0,0 +1,5 @@
+/Users/twilliams/.cdprct/Coding Practice/Rust/hello_cargo/target/debug/deps/libhello_cargo-d0f922fb585396f1.rmeta: src/main.rs
+
+/Users/twilliams/.cdprct/Coding Practice/Rust/hello_cargo/target/debug/deps/hello_cargo-d0f922fb585396f1.d: src/main.rs
+
+src/main.rs:
