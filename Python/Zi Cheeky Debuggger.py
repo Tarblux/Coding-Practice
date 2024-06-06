7ad5ffdac59c62d0ca2ff06180b7a4a7162e57e3
Tariq Williams <121918283+Tarblux@users.noreply.github.com>
2024-06-06 11:18:35 -0500

    aaa

--- Python/Zi Cheeky Debuggger.py
+++ Python/Zi Cheeky Debuggger.py
@@ -1,46 +1,12 @@
-class Solution:
-    def threeSum(nums):
-            
-        result = []
+def binary_search(array ,target) -> int:
 
-        nums.sort()
-
-        for i in range (0,len(nums)) : 
-
-            if i > 0 and nums[i] == nums[i-1] : 
-
-                continue
-
-            right = len(nums) - 1
-
-            left = i + 1
-
-            while left < right : 
-
-                cur_sum = nums[i] + nums[left] + nums[right]
-
-                if cur_sum == 0 :
-
-                    result.append([nums[i],nums[left],nums[right]])
-
-                    # left += 1
-                    # right -= 1
-                    
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
-                if cur_sum < 0 :
-
-                    left += 1
-
-                if cur_sum > 0 :
-
-                    right -= 1
-                    
-        return result
-    
-print (Solution.threeSum([-1,0,1,2,-1,-4]))
-    
+    left, right = 0, len(array) - 1
+    while left < right:
+        mid = left + (right - left) // 2
+        if mid == target :
+            return mid
+        elif mid < target:
+            right = mid - 1 
+        else:
+            left = mid + 1
+    return left
