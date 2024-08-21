e4e176b095ec147fce8491d098585fbaa0ba5b1b
Tariq Williams <121918283+Tarblux@users.noreply.github.com>
2024-08-21 18:33:40 -0500

    More Graph Practice

--- Python/Zi Cheeky Debuggger.py
+++ Python/Zi Cheeky Debuggger.py
@@ -1,12 +1,57 @@
-def binary_search(array ,target) -> int:
+from collections import deque
 
-    left, right = 0, len(array) - 1
-    while left < right:
-        mid = left + (right - left) // 2
-        if mid == target :
-            return mid
-        elif mid < target:
-            right = mid - 1 
-        else:
-            left = mid + 1
-    return left
+class solution:
+    
+    def __init__(self):
+        
+        self.visited = set()
+        
+    def neighbors(self,matrix,r,c):
+        
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        
+        for dr , dc in directions:
+            
+            nr = r + dr
+            nc = c + dc
+            
+            if  0 <= nr < len(matrix) and 0 <= nc < len(matrix[0]):
+                yield (nr,nc)
+    
+    def bfs(self,matrix):
+        
+        output = 0
+        
+        if not matrix:
+            return None
+        
+        queue = deque([(0,0)])
+        
+        while queue:
+            
+            r , c = queue.popleft()
+            
+            if (r,c) in self.visited:
+                continue
+            
+            self.visited.add((r,c))
+            
+            if matrix[r][c] == 1:
+                output += 1
+            
+            for n in self.neighbors(matrix,r,c):
+                
+                if n not in self.visited:
+                    queue.append(n)
+                
+        return output
+                
+        
+        
+data  = [[0,0,1,1],
+         [0,0,0,1],
+         [0,0,0,1],
+         [0,0,0,0]]   
+
+test = solution() 
+test.bfs(data)
