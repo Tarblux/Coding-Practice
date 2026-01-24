fc1dde473e238e3edeffe6a215810fa2610a29cf
Tariq Williams <121918283+Tarblux@users.noreply.github.com>
2026-01-23 20:57:26 -0600

    Finished Switch Statements

--- C and C++/learn-c-the-hard-way-lectures/ex10/ex10_t.c
+++ C and C++/learn-c-the-hard-way-lectures/ex10/ex10_t.c
@@ -17,57 +17,65 @@ a =  (66 + 32) - 97  % 122 = 1
 
 */ 
 
+// int static len(
+
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2){
+	if (argc < 2){
 
 		printf("ERROR: You need only one argument. \n");
 		return 1;
 	}
 
 	int i = 0;
-	for(i = 0; argv[1][i] != '\0' ; i++){
-		char letter = argv[1][i];
+	int j = 0;
+	char letter;
 
-		int asc_val = (int) letter;
+	for (j = 1 ; j < argc ; j++){ 
 
-		if (asc_val < 97){
-			asc_val = ((int) letter + 32 );
+		for(i = 0 ,letter = argv[j][0]; letter != '\0' ; i++ , letter = argv[j][i]){
+
+			int asc_val = (int) letter;
+
+			if (asc_val < 97){
+				asc_val = ((int) letter + 32 );
+			}
+
+			switch (asc_val){
+				case 97:
+					printf("%d: 'A'\n",i);
+					break;
+
+				case 101:
+					printf("%d: 'E'\n",i);
+					break;
+
+				case 105:
+					printf("%d: 'I'\n",i);
+					break;
+
+				case 111:
+					printf("%d: 'O'\n",i);
+					break;
+
+				case 117:
+					printf("%d: 'U'\n",i);
+					break;
+
+				case 'y':
+				case 'Y':
+					
+					if (i>2){
+						printf("%d: 'Y'\n",i);
+					}
+					break;
+
+				default:
+					printf("%d: %c is not a vowel \n",i,letter);
+
+			}
 		}
-
-		switch (asc_val){
-			case 97:
-				printf("%d: 'A'\n",i);
-				break;
-
-			case 101:
-				printf("%d: 'E'\n",i);
-				break;
-
-			case 105:
-				printf("%d: 'I'\n",i);
-				break;
-
-			case 111:
-				printf("%d: 'O'\n",i);
-				break;
-
-			case 117:
-				printf("%d: 'U'\n",i);
-				break;
-
-			case 'y':
-			case 'Y':
-				
-				if (i>2){
-					printf("%d: 'Y'\n",i);
-				}
-				break;
-
-			default:
-				printf("%d: %c is not a vowel \n",i,letter);
-
-		}
+		printf("\n");
 	}
 }
